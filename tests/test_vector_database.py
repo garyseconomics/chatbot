204f4ff204d790e77fb28b7e31772bf29232c126
Adavideo <adavideo@gmail.com>
2025-10-23 10:54:44 +0200

    Improved testing for generate database

--- tests/test_vector_database.py
+++ tests/test_vector_database.py
@@ -1,15 +1,30 @@
 import pytest
+import chromadb
 from langchain_chroma import Chroma
 from vector_database.vector_database import *
 
 test_database_path = './tests/chroma_langchain_db'
+collection_name = 'youtube_videos'
+client = chromadb.PersistentClient(path=test_database_path)
+
 
 def test_create_database():
     vector_store = create_vector_database(test_database_path)
     # Verify the returned object is an instance of Chroma
     assert isinstance(vector_store, Chroma)
+    collection = client.get_collection(collection_name)
+    # Verify the collection has been created
+    assert isinstance(collection, chromadb.Collection)
+    assert collection.count() > 0
 
 def test_generate_db_with_documents():
+    client.delete_collection(collection_name)
     files_list = ["tests/sample.srt"]
     vector_store = generate_db_with_documents(test_database_path, files_list)
-    assert isinstance(vector_store, Chroma)
+    collection = client.get_collection(collection_name)
+    # Verify the collection has been created
+    assert isinstance(collection, chromadb.Collection)
+    # Verify the collection has two items
+    assert collection.count() == 2
+    documents = collection.peek()["documents"]
+    assert documents[0] == '''Okay. Welcome back to Gary’s Economics. Today we are going to introduce the first part of our online course. Video number one, which is going to be What is Wealth'''
