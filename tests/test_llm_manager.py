3667816387a033ece4dd68ba30e7c374d0648d30
Adavideo <adavideo@gmail.com>
2025-10-24 08:33:38 +0200

    Added rag prompt template

--- tests/test_llm_manager.py
+++ tests/test_llm_manager.py
@@ -1,6 +1,8 @@
 import pytest
 import langchain_core
+from langchain_core.prompts import ChatPromptTemplate
 from llm.llm_manager import *
+from llm.prompt_template import get_rag_prompt
 
 def test_get_llm_client():
 	llm = get_llm_client()
@@ -11,4 +13,18 @@ def test_llm_chat_simple_prompt():
 	prompt = "Hello"
 	response = llm_chat(llm, prompt)
 	assert isinstance(response, langchain_core.messages.ai.AIMessage)
+
+def test_prompt_template():
+	prompt = get_rag_prompt()
+	assert isinstance(prompt, ChatPromptTemplate)
+	messages = prompt.invoke({"question": "Who are you?", "context": "You are an AI assistant."})
+	print(type(messages))
+	assert isinstance(messages, langchain_core.prompt_values.ChatPromptValue)
+
+def test_llm_chat_with_prompt_template():
+    llm = get_llm_client()
+    prompt = ChatPromptTemplate.from_messages([("human", "Hello")])
+    messages = prompt.invoke({"question": "Who are you?", "context": "You are an AI assistant."})
+    response = llm_chat(llm, messages)
+    assert isinstance(response, langchain_core.messages.ai.AIMessage)
 
