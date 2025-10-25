cebf234704d89617f89aa37244403d0253433f95
Adavideo <adavideo@gmail.com>
2025-10-25 08:44:28 +0200

    Telegram bot 0.1 - It listen to your questions and tell you what your question is.

--- bots/telegram_bot.py
+++ bots/telegram_bot.py
@@ -1,6 +1,6 @@
 import logging, os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 from dotenv import load_dotenv
 
 welcome_message = "This is the chatbot for Gary's Economics YouTube channel. You can ask me questions, and I will answer them using the content from our videos."
@@ -18,6 +18,12 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=welcome_message)
 
+# Question funtion. This funtion will answer when a user sends a text message to the bot.
+async def question(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    question = update.message.text
+    answer = f"You asked: {question}"
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+
 if __name__ == '__main__':
     # Start the application, using the telegram token to connect to the bot
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
@@ -25,6 +31,10 @@ if __name__ == '__main__':
     # Calls the start handler that listen for telegram users starting a new conversation with the bot
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+    
+    # Calls the update handler that listen for telegram users sending a text message on an already open chat
+    question_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), question)
+    application.add_handler(question_handler)
     
     # Keeps the bot alive and listening
     application.run_polling()
