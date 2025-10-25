0a223486ba746fcad9112c732ae2f8f558f91082
Adavideo <adavideo@gmail.com>
2025-10-25 10:00:17 +0200

    Telegram bot now answer questions using the RAG search

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,6 +2,8 @@ import logging, os
 from telegram import Update
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 from dotenv import load_dotenv
+from rag.RAG_manager import RAG_query
+from config import show_logs
 
 welcome_message = "This is the chatbot for Gary's Economics YouTube channel. You can ask me questions, and I will answer them using the content from our videos."
 
@@ -21,20 +23,23 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # Question funtion. This funtion will answer when a user sends a text message to the bot.
 async def question(update: Update, context: ContextTypes.DEFAULT_TYPE):
     question = update.message.text
-    answer = f"You asked: {question}"
+    rag_answer = RAG_query(question)
+    answer = rag_answer["answer"]
+    if show_logs:
+        print(rag_answer)
     await context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
 
 if __name__ == '__main__':
     # Start the application, using the telegram token to connect to the bot
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-    
+
     # Calls the start handler that listen for telegram users starting a new conversation with the bot
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
-    
+
     # Calls the update handler that listen for telegram users sending a text message on an already open chat
     question_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), question)
     application.add_handler(question_handler)
-    
+
     # Keeps the bot alive and listening
     application.run_polling()
