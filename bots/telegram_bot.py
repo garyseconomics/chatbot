ae9662961c0af88bd8a60513d2df0ce9e8258be6
Adavideo <adavideo@gmail.com>
2025-10-25 08:11:05 +0200

    Added comments to improve clarity on how the telegram bot works

--- bots/telegram_bot.py
+++ bots/telegram_bot.py
@@ -14,13 +14,17 @@ logging.basicConfig(
 load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
+# Start funtion. This function will be called every time the Bot receives a Telegram message that contains the /start command.
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=welcome_message)
 
 if __name__ == '__main__':
+    # Start the application, using the telegram token to connect to the bot
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
     
+    # Calls the start handler that listen for telegram users starting a new conversation with the bot
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
     
+    # Keeps the bot alive and listening
     application.run_polling()
