68bc7e116eebbd9baea5e6acc257eaf49bec257b
Adavideo <adavideo@gmail.com>
2025-10-30 07:06:07 +0100

    Returns several videos instead of only one

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,6 @@
 from rag.RAG_manager import RAG_query
 from config import show_logs
-from video_links import get_video_link
+from video_links import get_video_link, videos_text_for_chat
 
 print("This is the chatbot for Gary's Economics YouTube channel. You can ask me questions, and I will answer them using the content from our videos.")
 question = input("Your question: ")
@@ -8,6 +8,6 @@ response = RAG_query(question)
 if show_logs:
     print(response)
 print(response["answer"])
-video_link = get_video_link(response["context"])
-if video_link:
-    print(f"More information on this video: {video_link}")
+video_links = get_video_link(response["context"])
+video_text = videos_text_for_chat(video_links)
+print(video_text)
--- video_links.py
+++ video_links.py
@@ -1,12 +1,27 @@
 from config import documents_directory
 
+# Get the list of youtube videos from the context
 def get_video_link(context):
     if not context:
+        return []
+    videos_list = []
+    for doc in context:
+        file_source = doc.metadata["source"]
+        # Takes the video id from the name of the file
+        video_id = file_source.strip(documents_directory).split('_')[0]
+        video_url = "https://www.youtube.com/watch?v="+video_id
+        if not video_url in videos_list:
+            videos_list.append(video_url)
+    return videos_list
+
+# Get the list of videos and returns a text for the chatbots
+def videos_text_for_chat(video_links):
+    if not video_links:
         return ""
-    # We only take the link from the first document (in case there are more than one)
-    document = context[0]
-    file_source = document.metadata["source"]
-    # Takes the video id from the name of the file
-    video_id = file_source.strip(documents_directory).split('_')[0]
-    video_url = "https://www.youtube.com/watch?v="+video_id
-    return video_url
+    if len(video_links) == 1:
+        videos_text = f"More information on this video: {video_links[0]}"
+    else:
+        videos_text = ("More information on this videos:\n")
+        for video in video_links:
+            videos_text += video + "\n"
+    return videos_text
