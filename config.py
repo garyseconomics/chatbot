6df5ee7a5c616d9c9b771e1f41ba68fc6808b962
Adavideo <adavideo@gmail.com>
2025-11-07 09:29:33 +0100

    Adding the list of available models to config for easier access

--- config.py
+++ config.py
@@ -8,6 +8,11 @@ show_logs = True
 discord_channel = "youtube-chatbot"
 embedding_model = "qwen3-embedding:8b"
 use_remote_llm = True
+# AVAILABLE MODELS:
+# Mistral: "dolphin-mistral:7b","mistral-small3.1:24b"
+# Alibaba: "qwen3:4b","qwen3:8b","qwen3:14b","qwen3:30b","qwen3:32b"
+# Meta: "llama3.1:8b","llama3.3:70b"
+# Open AI: "gpt-oss:20b"
 remote_llm = "qwen3:4b"
 local_llm = "qwen3:4b"
 video_ids_separator = "__"
