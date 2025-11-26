e9d094da71aa26889c76e4da43c167908e0425c3
Carmen <1241692+Adavideo@users.noreply.github.com>
2025-11-26 13:05:52 +0100

    Changed the LLM to Qwen3 32B
    After testing several models, Qwen3 in the 32B size has been chosen for the chatbot.

--- config.py
+++ config.py
@@ -8,12 +8,7 @@ show_logs = True
 discord_channel = "youtube-chatbot"
 embedding_model = "qwen3-embedding:8b"
 use_remote_llm = True
-# AVAILABLE MODELS:
-# Mistral: "dolphin-mistral:7b","mistral-small3.1:24b"
-# Alibaba: "qwen3:4b","qwen3:8b","qwen3:14b","qwen3:30b","qwen3:32b"
-# Meta: "llama3.1:8b","llama3.3:70b"
-# Open AI: "gpt-oss:20b"
-remote_llm = "qwen3:4b"
+remote_llm = "qwen3:32b"
 local_llm = "qwen3:4b"
 video_ids_separator = "__"
 bot_greeting = "Hello. This is Garys Economics Youtube chatbot. You can ask me questions and I'll answer them using the content of our videos."
