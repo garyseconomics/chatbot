06cefba73c28ead751d83b7944cff2aea1967d2b
Carmen <1241692+Adavideo@users.noreply.github.com>
2025-11-26 16:43:22 +0100

    Changed the local LLM to Qwen3 8B

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ discord_channel = "youtube-chatbot"
 embedding_model = "qwen3-embedding:8b"
 use_remote_llm = True
 remote_llm = "qwen3:32b"
-local_llm = "qwen3:4b"
+local_llm = "qwen3:8b"
 video_ids_separator = "__"
 bot_greeting = "Hello. This is Garys Economics Youtube chatbot. You can ask me questions and I'll answer them using the content of our videos."
