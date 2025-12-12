8c4a32be782bd41b2861aadc2e8d5c7b81066f2f
Adavideo <adavideo@gmail.com>
2025-12-12 14:40:27 +0100

    Added new question for testing

--- questions.py
+++ questions.py
@@ -2,4 +2,5 @@ questions_list = ["What is wealth?",
 "If we tax the rich, will they leave?",
 "How to fix the economy?",
 "Who is Gary?",
-"Tell me about the channel"]
+"Tell me about the channel",
+"What is Gary's opinion about the Labour party?"]
